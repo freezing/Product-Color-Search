329117f5583900437c46da1cea97da1894346a3b
Nikola Stojiljkovic <nikolavla@gmail.com>
2015-07-19 18:30:49 +0200

    Implemented bilateral filtering and kmeans clustering

--- src/processing/ColorExtractorImpl.java
+++ src/processing/ColorExtractorImpl.java
@@ -6,13 +6,14 @@ import org.opencv.core.Mat;
 import org.opencv.core.Rect;
 import org.opencv.imgproc.Imgproc;
 
+import algorithm.filters.ImageFilters;
 import algorithm.segmentation.BackgroundSegmentation;
 import algorithm.segmentation.CannyBackgroundSegmentation;
+import constants.Constants;
 import model.Color;
 import utils.Utils;
 
 public class ColorExtractorImpl implements ColorExtractor {
-
 	private BackgroundSegmentation backgroundSegmentation;
 	
 	public ColorExtractorImpl() {
@@ -21,28 +22,46 @@ public class ColorExtractorImpl implements ColorExtractor {
 	
 	@Override
 	public List<Color> run(Mat image) {
+		Utils.displayImage(image, false);
 		// 1. Convert to LAB color space
 		Mat labImage = new Mat();
 		Imgproc.cvtColor(image, labImage, Imgproc.COLOR_BGR2Lab);
 		
+		Mat filtered = prefiltering(labImage);
+		
 		// 2. Detect foreground pixels
-		boolean[][] segmentationMask = backgroundSegmentation.run(labImage);
+		boolean[][] segmentationMask = backgroundSegmentation.run(filtered);
 
 		// 3. Find bounding box
 		Rect boundingBox = findBoundingBox(segmentationMask);
 
 		// 4. Crop image
-		Mat cropped = labImage.submat(boundingBox);
+		Mat cropped = filtered.submat(boundingBox);
 		
-		// 5. Apply bilateral filtering
-		Mat filtered = new Mat();
-		Imgproc.bilateralFilter(cropped, filtered, 5, 100, 150);
-		
-		Utils.displayImage(filtered, true);
-		
-		// 6. Apply K-means clustering
+		Utils.displayImage(cropped, true);
 		
 		return null;
+	}
+
+	private Mat prefiltering(Mat image) {
+		// Apply bilateral filtering
+		Mat filtered = applyBilateralFiltering(image);
+		
+		// Apply K-means clustering
+		filtered = applyKMeansClustering(filtered);
+		
+		Utils.displayImage(filtered, true);
+		return filtered;
+	}
+
+	private Mat applyBilateralFiltering(Mat image) {
+		Mat filtered = new Mat();
+		Imgproc.bilateralFilter(image, filtered , 5, 100, 150);
+		return filtered;
+	}
+
+	private Mat applyKMeansClustering(Mat image) {
+		return ImageFilters.kmeansClustering(image, Constants.CLUSTER_COUNT, Constants.ATTEMPTS);
 	}
 
 	private Rect findBoundingBox(boolean[][] segmentationMask) {		
